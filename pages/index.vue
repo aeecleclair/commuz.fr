7306d1a0c2e99c2e1292b3dd7e2364ca9a42fc95
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-02-02 10:25:40 +0100

    Quote and padding in main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
     <title>La Commuz'</Title>
-    <Meta
-name="description"
+    <Meta name="description"
       content="La Commuz' est une comédie musicale organisée chaque année depuis 20 ans par une équipe de près de 100 étudiants de l'École Centrale de Lyon et de l'EMLyon." />
 
     <IndexVideoTitle />
 
-
-    <div class="flex flex-col gap-4 px-40 snap-start">
+    <div class="flex flex-col gap-40 px-40 mt-30 pt-10 mb-40 snap-start">
 
 
 
@@ -31,79 +29,87 @@ name="description"
         </div>
       </UCard>
 
-      <HeroCarousel title=" La Commuz', c'est quoi ?" :images="[
-        '/AaZ/IMG_1547.jpg',
-        '/AaZ/IMG_2246.jpg',
-        '/AaZ/IMG_5434.jpg',
-        '/AaZ/IMG_5917.jpg',
-        '/AaZ/IMG_6097.jpg',
-        '/AaZ/IMG_6154.jpg',
-      ]">
-        <p>
-          La Commuz' est une comédie musicale organisée chaque année depuis 20 ans par une équipe de près de 100
-          étudiant&#183;e&#183;s de l'École Centrale de Lyon et de l'emlyon business school.
-        </p>
-        <p>
-          La Commuz’ réunit chaque année plus de 1 200 spectateurs lors de quatre soirées de représentations au mois de
-          mars.
-        </p>
-      </HeroCarousel>
+      <div class="flex flex-col gap-20">
 
-      <HeroCarousel title="Un spectacle imaginé de A à Z" :reversed="true" :images="[
-        '/AaZ/IMG_1547.jpg',
-        '/AaZ/IMG_2246.jpg',
-        '/AaZ/IMG_5434.jpg',
-        '/AaZ/IMG_5917.jpg',
-        '/AaZ/IMG_6097.jpg',
-        '/AaZ/IMG_6154.jpg',
-      ]">
-        <p>
-          De l'élaboration du scénario en passant par les chorégraphies, la composition et l'orchestration des musiques,
-          la mise en place de l'éclairage et du son, la création des costumes et des décors…
-        </p>
-        <p>
-          Tous les corps de métier du théâtre, du chant et de la danse sont réunis pour créer ce spectacle qui se veut
-          d'une qualité professionnelle.
-        </p>
-      </HeroCarousel>
+        <HeroCarousel title=" La Commuz', c'est quoi ?" :images="[
+          '/AaZ/IMG_1547.jpg',
+          '/AaZ/IMG_2246.jpg',
+          '/AaZ/IMG_5434.jpg',
+          '/AaZ/IMG_5917.jpg',
+          '/AaZ/IMG_6097.jpg',
+          '/AaZ/IMG_6154.jpg',
+        ]">
+          <p>
+            La Commuz' est une comédie musicale organisée chaque année depuis 20 ans par une équipe de près de 100
+            étudiant&#183;e&#183;s de l'École Centrale de Lyon et de l'emlyon business school.
+          </p>
+          <p>
+            La Commuz’ réunit chaque année plus de 1 200 spectateurs lors de quatre soirées de représentations au mois
+            de
+            mars.
+          </p>
+        </HeroCarousel>
 
-      <HeroCarousel title="Deux écoles, un projet commun" :images="[
-        '/AaZ/IMG_1547.jpg',
-        '/AaZ/IMG_2246.jpg',
-        '/AaZ/IMG_5434.jpg',
-        '/AaZ/IMG_5917.jpg',
-        '/AaZ/IMG_6097.jpg',
-        '/AaZ/IMG_6154.jpg',
-      ]">
-        <p>
-          100 élèves aux talents variés issus de l'emlyon business school et de l'École Centrale de Lyon travaillent
-          pendant un an pour offrir un spectacle unique.
-        </p>
-        <p>
-          Du recrutement de l'équipe, à la fermeture du dernier rideau, chaque membre contribue au caractère authentique
-          et inédit de ce grand projet.
-        </p>
-      </HeroCarousel>
+        <HeroCarousel title="Un spectacle imaginé de A à Z" :reversed="true" :images="[
+          '/AaZ/IMG_1547.jpg',
+          '/AaZ/IMG_2246.jpg',
+          '/AaZ/IMG_5434.jpg',
+          '/AaZ/IMG_5917.jpg',
+          '/AaZ/IMG_6097.jpg',
+          '/AaZ/IMG_6154.jpg',
+        ]">
+          <p>
+            De l'élaboration du scénario en passant par les chorégraphies, la composition et l'orchestration des
+            musiques,
+            la mise en place de l'éclairage et du son, la création des costumes et des décors…
+          </p>
+          <p>
+            Tous les corps de métier du théâtre, du chant et de la danse sont réunis pour créer ce spectacle qui se veut
+            d'une qualité professionnelle.
+          </p>
+        </HeroCarousel>
+
+        <HeroCarousel title="Deux écoles, un projet commun" :images="[
+          '/AaZ/IMG_1547.jpg',
+          '/AaZ/IMG_2246.jpg',
+          '/AaZ/IMG_5434.jpg',
+          '/AaZ/IMG_5917.jpg',
+          '/AaZ/IMG_6097.jpg',
+          '/AaZ/IMG_6154.jpg',
+        ]">
+          <p>
+            100 élèves aux talents variés issus de l'emlyon business school et de l'École Centrale de Lyon travaillent
+            pendant un an pour offrir un spectacle unique.
+          </p>
+          <p>
+            Du recrutement de l'équipe, à la fermeture du dernier rideau, chaque membre contribue au caractère
+            authentique
+            et inédit de ce grand projet.
+          </p>
+        </HeroCarousel>
+
+      </div>
+
+      <!-- Témoignages -->
+      <UCard
+        class="group py-10 flex flex-col items-center justify-center bg-gradient-to-tr from-commuz-900 to-commuz-500 border-none">
+        <IndexQuote name="Frank Debouck &ndash; Directeur de l'École Centrale de Lyon entre 2011 et 2021"
+          img-src="/images/homepage/debouck.jpg">
+          J'ai pris hier soir un grand plaisir, le spectacle est de grande qualité, son, lumière, costumes magnifiques,
+          décors, le tout au service d'artistes de talents : musiciens, danseurs, chanteurs, acteurs-chanteurs.
+        </IndexQuote>
+
+        <div class="h-10"></div>
+
+        <IndexQuote name="Pascal Ray &ndash; Directeur de l'École Centrale de Lyon depuis 2021"
+          img-src="/images/homepage/PascalRay.jpg">
+          Tout est parfaitement pensé et orchestré pour produire un spectacle unique et d'une qualité exceptionnelle.
+          N'hésitez-pas à rejoindre l'aventure Commuz' en soutenant le talent de nos étudiant.e.s. Et n'oubliez-pas de
+          venir les applaudir sur scène. Merci pour eux !
+        </IndexQuote>
+      </UCard>
     </div>
 
-    <!-- Témoignages -->
-    <section id="testimonial">
-      <IndexQuote
-name="Frank Debouck &ndash; Directeur de l'École Centrale de Lyon entre 2011 et 2021"
-        img-src="/images/homepage/debouck.jpg">
-        J'ai pris hier soir un grand plaisir, le spectacle est de grande qualité, son, lumière, costumes magnifiques,
-        décors, le tout au service d'artistes de talents : musiciens, danseurs, chanteurs, acteurs-chanteurs.
-      </IndexQuote>
-
-      <IndexQuote
-name="Pascal Ray &ndash; Directeur de l'École Centrale de Lyon depuis 2021"
-        img-src="/images/homepage/PascalRay.jpg">
-        Tout est parfaitement pensé et orchestré pour produire un spectacle unique et d'une qualité exceptionnelle.
-        N'hésitez-pas à rejoindre l'aventure Commuz' en soutenant le talent de nos étudiant.e.s. Et n'oubliez-pas de
-        venir les applaudir sur scène. Merci pour eux !
-      </IndexQuote>
-
-    </section>
     <!-- Sponsors -->
     <Sponsors />
   </div>
@@ -114,34 +120,3 @@ definePageMeta({
   layout: "home",
 });
 </script>
-
-<style lang="scss">
-#testimonial {
-  width: 90%;
-
-  @media only screen and (min-width: 768px) {
-    width: 80%;
-  }
-
-  @media only screen and (min-width: 992px) {
-    width: 70%;
-  }
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 2em;
-
-  @media only screen and (min-width: 768px) {
-    padding: 4em;
-  }
-
-  margin: 3em 0;
-  background: linear-gradient(100deg, #de8406, #e72203 100%);
-
-
-
-
-}
-</style>
