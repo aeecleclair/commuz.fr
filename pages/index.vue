8b110ab15426859199e32389d1d22b65dfa3499b
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-04-21 18:55:49 +0200

    Animated gradient for Tailwind v4

--- pages/index.vue
+++ pages/index.vue
@@ -8,18 +8,18 @@
 
     <div class="flex flex-col gap-40 px-10 md:px-40 mt-30 pt-10 mb-40 snap-start">
 
-      <UCard class="group py-10 my-5" :ui="{ background: 'bg-white dark:bg-white' }">
-        <div class="flex flex-col items-center justify-center text-(--ui-primary)">
+      <UCard class="group py-10 my-5">
+        <div class=" flex flex-col items-center justify-center text-(--ui-primary) gap-4">
           <span class="text-xl text-neutral-700">La Commuz' présente son spectacle 2025</span>
 
           <span
-            class="text-5xl font-bold rounded-xl transition-all duration-500 bg-gradient-to-tl from-fuchsia-800 via-blue-500 to-cyan-500 bg-size-200 bg-pos-0 text-transparent group-hover:bg-pos-100 bg-clip-text mt-3">
+            class="text-6xl font-bold rounded-xl transition-all duration-500 bg-gradient-to-tl from-fuchsia-800 via-blue-500 to-cyan-500 bg-[length:200%_200%] bg-[position:0%_0%]  text-transparent group-hover:bg-[position:100%_100%] bg-clip-text mt-3">
             FACE AU MUR
           </span>
 
-          <NuxtLink to="/spectacle/2025" class="text-neutral-400 hover:text-neutral-700 mt-4">
-            <span class="flex flex-row items-center space-x-2">
-              <span>En savoir plus </span>
+          <NuxtLink to="/spectacle/2025" class="mt-4">
+            <span class="flex flex-row items-center justify-center gap-2 text-neutral-400 hover:text-neutral-700">
+              <span>En savoir plus</span>
               <UIcon name="i-heroicons-arrow-long-right-solid" />
             </span>
           </NuxtLink>
