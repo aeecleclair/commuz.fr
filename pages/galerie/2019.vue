84348957dda5fc3cc182f8f371cf0a29b2947712
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2026-02-14 15:35:38 +0100

    Exp Nuxt4

--- pages/galerie/2019.vue
+++ pages/galerie/2019.vue
@@ -15,7 +15,7 @@
 <script>
 import VueEasyLightbox from 'vue-easy-lightbox'
 
-const imagesFiles = import.meta.glob('public/images/galeries/2019/*')
+const imagesFiles = import.meta.glob('./public/images/galeries/2019/*')
 
 const imagesPaths = [];
 
