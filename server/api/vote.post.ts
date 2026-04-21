a5af4eb1b62d042247a0dbbe4f839b1a3392b874
Ohkaÿe <the.misterfavo@gmail.com>
2026-04-21 19:40:14 +0200

    Static to dynamic
    try to go from static to dynamic

--- server/api/vote.post.ts
+++ server/api/vote.post.ts
@@ -3,7 +3,7 @@
 import { appendFileSync } from 'fs';
 import { resolve } from 'path';
 
-const VOTES_FILE = resolve('./votes.ndjson');
+const VOTES_FILE = resolve('/app/data/votes.ndjson');
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
